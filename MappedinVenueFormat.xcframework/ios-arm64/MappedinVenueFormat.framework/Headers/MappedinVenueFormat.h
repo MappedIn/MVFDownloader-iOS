5759510ec29b4f07d1240c33aa7656e4be28b062
daniellewang-mi <danielle.wang@mappedin.ca>
2020-06-16 14:00:29 -0400

    Change progress handler

--- MappedinVenueFormat.xcframework/ios-arm64/MappedinVenueFormat.framework/Headers/MappedinVenueFormat.h
+++ MappedinVenueFormat.xcframework/ios-arm64/MappedinVenueFormat.framework/Headers/MappedinVenueFormat.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for MappedinVenueFormat
-FOUNDATION_EXPORT double MVFDownloaderVersionNumber;
+FOUNDATION_EXPORT double MappedinVenueFormatVersionNumber;
 
 //! Project version string for MappedinVenueFormat.
-FOUNDATION_EXPORT const unsigned char MVFDownloaderVersionString[];
+FOUNDATION_EXPORT const unsigned char MappedinVenueFormatVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MVFDownloader/PublicHeader.h>
 
